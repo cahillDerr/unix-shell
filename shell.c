39735efc187307783769f0d00039c9ed7f40f42b
cahillDerr <cahill.der@northeastern.edu>
2026-02-25 00:00:49 +0000

    first

--- shell.c
+++ shell.c
@@ -1,6 +1,26 @@
 
+#define Max_Line 255
+
+
+
 int main(int argc, char **argv) {
 
   // TODO: Implement your shell's main
-  return 0;
+  (void) argc;
+  (void) argv;
+  print("Welcome to mini-shell.\n")
+
+  while (1) {
+    printf("shell $");
+    fflush(stdout);
+
+    if (fgets(line, sizeof(line), stdin) == NULL) {
+      printf("\nBye Bye.\n")
+      break;
+    }
+    size_t len = strlen(line);
+    
+  }
+
+
 }
